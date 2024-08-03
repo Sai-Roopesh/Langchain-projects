7aa4214e8652233d97935afb0b5de3cbd544f881
Sai Roopesh <sairoopesh21@gmail.com>
2024-08-03 23:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-## langsmith tracking
 
-os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
-os.environ['LANGCHAIN_TRACING_V2'] = "true"
-os.environ['LANGCHAIN_PROJECT'] = 'Q&A Chatbot'
 
 # prompt
 
